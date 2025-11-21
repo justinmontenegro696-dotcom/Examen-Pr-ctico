c791a930ab974ffaebe0dd05500b5c9ae7a9c3dd
justinmontenegro696-dotcom <justin.montenegro696@ist17dejulio.edu.ec>
2025-11-21 08:27:36 -0500

    Segundo commit

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,4 +1,6 @@
-document.getElementById("formRegistro").addEventListener("submit", function(event) {
+document
+  .getElementById("formRegistro")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     let nombreInput = document.getElementById("nombre");
@@ -26,41 +28,38 @@ document.getElementById("formRegistro").addEventListener("submit", function(even
     let soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$/;
 
     if (nombre.length < 3) {
-        errorNombre.textContent = "El nombre debe tener al menos 3 caracteres.";
-        nombreInput.focus();
-        valido = false;
+      errorNombre.textContent = "El nombre debe tener al menos 3 caracteres.";
+      nombreInput.focus();
+      valido = false;
     } else if (!soloLetras.test(nombre)) {
-        errorNombre.textContent = "El nombre solo debe contener letras, no números.";
-        nombreInput.focus();
-        valido = false;
+      errorNombre.textContent =
+        "El nombre solo debe contener letras, no números.";
+      nombreInput.focus();
+      valido = false;
     }
 
     let validadCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (valido && !validadCorreo.test(correo)) {
-        errorCorreo.textContent = "Ingrese un correo electrónico válido.";
-        correoInput.focus();
-        valido = false;
+      errorCorreo.textContent = "Ingrese un correo electrónico válido.";
+      correoInput.focus();
+      valido = false;
     }
 
     edad = Number(edad);
     if (valido && (isNaN(edad) || edad < 18 || edad > 99)) {
-        errorEdad.textContent = "La edad debe estar entre 18 y 99.";
-        edadInput.focus();
-        valido = false;
+      errorEdad.textContent = "La edad debe estar entre 18 y 99.";
+      edadInput.focus();
+      valido = false;
     }
 
     if (valido && lenguaje === "") {
-        errorLenguaje.textContent = "Debe seleccionar un lenguaje obligatorio.";
-        lenguajeInput.focus();
-        valido = false;
+      errorLenguaje.textContent = "Debe seleccionar un lenguaje obligatorio.";
+      lenguajeInput.focus();
+      valido = false;
     }
 
     if (valido) {
-        alert("Registro exitoso");
-        this.reset();
-    }   else {
-        alert("Complete todos lo campos");
+      alert("Registro exitoso");
+      this.reset();
     }
-});
-
-
+  });
